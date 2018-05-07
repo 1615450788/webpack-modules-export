3e6dea8cf011e2b5e32dec12d1c849b8f3344482
wangtao <1615450788@qq.com>
2018-05-07 10:52:09 +0800

    修复import后跟注释造成的解析异常

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
-const methods = require('./lib/methods');
 const filterObject = require('./lib/filterObject');
 
-function HelloWorldPlugin(options={}) {
+function HelloWorldPlugin({fileName,outputName,onComplete}) {
     // 使用配置（options）设置插件实例
-    this.fileName=options.fileName||'menu.js';
-    this.outputName=options.outputName||'menu.json';
+    this.fileName=fileName||'menu.js';
+    this.outputName=outputName||'menu.json';
+    this.onComplete=onComplete;
 }
 
 HelloWorldPlugin.prototype.apply = function(compiler) {
     compiler.plugin('emit', (compilation, callback)=>{
         let menuObj=[],menuText='';
-        compilation.chunks.forEach((chunk)=>{
+        if(this.fileName){
+          compilation.chunks.forEach((chunk)=>{
             chunk.modules.forEach((module)=>{
-                if(module&&module.resource&&(module.resource.indexOf(this.fileName)>=0)){
-                    module.fileDependencies.forEach((filepath)=>{
-                        let menu=fs.readFileSync(filepath).toString();
-                        menu=filterObject(menu);
-                        if(menu instanceof Array){
-                            menuObj=menuObj.concat(menu);
-                        }
-                    });
-                }
-
+              if(module&&module.resource&&(module.resource.indexOf(this.fileName)>=0)){
+                module.fileDependencies.forEach((filepath)=>{
+                  let menu=fs.readFileSync(filepath).toString();
+                  menu=filterObject(menu);
+                  if(menu instanceof Array){
+                    menuObj=menuObj.concat(menu);
+                  }
+                });
+              }
             });
-        });
+          });
+        }
+        if(this.onComplete instanceof Function){
+          menuObj=this.onComplete(menuObj);
+        }
         menuText=JSON.stringify(eval(menuObj),null,'  ');
         compilation.assets[this.outputName] = {
             source: () => {
